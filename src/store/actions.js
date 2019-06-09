a1286589155894ec0b721d8f9e9fa0810913dd44
ben <Astwood>
2019-06-09 17:47:11 +0100

    Removed svg logo boilerplate

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,17 +1,22 @@
-import {mapBoxKey, morrisonsApiKey} from "../tokens";
+import { mapBoxKey, morrisonsApiKey } from "../tokens";
 
-export const GET_GEOLOCATION = 'GET_GEOLOCATION';
-export const FETCH_STORE_LIST = 'FETCH_STORE_LIST';
-export const SELECT_STORE = 'SELECT_STORE';
+export const GET_GEOLOCATION = "GET_GEOLOCATION";
+export const FETCH_STORE_LIST = "FETCH_STORE_LIST";
+export const SELECT_STORE = "SELECT_STORE";
 
 export const fetchStoreList = (long, lat) => {
   return dispatch => {
-
     if (long === null || lat === null) {
-      return dispatch(fetchListError(`Geolcocation not supplied correctly. Longtitude: ${long}, latitude: ${lat}`))
+      return dispatch(
+        fetchListError(
+          `Geolcocation not supplied correctly. Longtitude: ${long}, latitude: ${lat}`
+        )
+      );
     }
 
-    return fetch(`https://api.morrisons.com/location/v2/stores?apikey=${morrisonsApiKey}&distance=50000&offset=0&storeformat=supermarket&lat=${lat}&lon=${long}&limit=5`)
+    return fetch(
+      `https://api.morrisons.com/location/v2/stores?apikey=${morrisonsApiKey}&distance=50000&offset=0&storeformat=supermarket&lat=${lat}&lon=${long}&limit=5`
+    )
       .then(response => response.json())
       .then(json => {
         dispatch(fetchListSuccess(json));
@@ -19,7 +24,7 @@ export const fetchStoreList = (long, lat) => {
       })
       .catch(err => fetchListError(err));
   };
-}
+};
 
 export const fetchListSuccess = stores => ({
   type: FETCH_STORE_LIST,
@@ -37,21 +42,27 @@ export const fetchListError = err => ({
 
 export const getGeolocation = inputedLocation => {
   return dispatch => {
-
-    return fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(inputedLocation)}.json?access_token=${mapBoxKey}&limit=5`)
+    return fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        inputedLocation
+      )}.json?access_token=${mapBoxKey}&limit=5`
+    )
       .then(response => response.json())
       .then(json => {
         dispatch(getGeolocationSuccess(json));
         return json;
       })
       .then(jsonData => {
-
         // TODO: Currently only first item of returned locations array is used. Extend
         // so that if there are multiple options then the user has a choice.
-        return dispatch(fetchStoreList(jsonData.features[0].center[0], jsonData.features[0].center[1]))
+        return dispatch(
+          fetchStoreList(
+            jsonData.features[0].center[0],
+            jsonData.features[0].center[1]
+          )
+        );
       })
       .catch(err => getGeolocationError(err));
-
   };
 };
 
