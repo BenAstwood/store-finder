477fa708f852ced4abd9d3ee38429e5ad4eb4ac9
ben <Astwood>
2019-06-09 17:54:09 +0100

    Updated FindStores container
    Includes more reusable components.

--- src/containers/FindStores.js
+++ src/containers/FindStores.js
@@ -1,51 +1,77 @@
-import React, {Component} from 'react';
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getGeolocation } from "../store/actions";
 
-import {googleMapsKey} from "../tokens";
+import { googleMapsKey } from "../tokens";
 
-import Header from '../components/Header';
-import SearchForm from '../components/SearchForm'
-import Input from '../components/Input'
-import StoreResults from '../components/StoreResults'
+import SearchForm from "./SearchForm";
+
+import Header from "../components/Header/Header";
+import StoreResults from "../components/StoreResults/StoreResults";
+import LayoutContainer from "../components/LayoutContainer/LayoutContainer";
 
 class FindStores extends Component {
-
   createMapUrl = (searchLocation, markers) => {
-    const markerCheck = markers[0].location && markers[0].location.latitude
-      ? markers.map(({
-        location
-      }, index) => `&markers=color:green%7Clabel:${index + 1}%7C${location.latitude},${location.longitude}`).join('')
-      : '';
+    const markerCheck =
+      markers[0].location && markers[0].location.latitude
+        ? markers
+            .map(
+              ({ location }, index) =>
+                `&markers=color:green%7Clabel:${index + 1}%7C${
+                  location.latitude
+                },${location.longitude}`
+            )
+            .join("")
+        : "";
 
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${encodeURIComponent(searchLocation)}&zoom=11&size=600x300&maptype=roadmap${markerCheck}&key=${googleMapsKey}`
-  }
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${encodeURIComponent(
+      searchLocation
+    )}&zoom=11&size=600x600&maptype=roadmap${markerCheck}&key=${googleMapsKey}`;
+  };
+
+  formSubmitHandler = searchString => {
+    this.props.dispatch(getGeolocation(searchString));
+  };
 
   render() {
-
     let mapUrl = null;
-    const {stores, searchLocation} = this.props;
+    const { stores, searchLocation } = this.props;
 
     if (searchLocation.name && Array.isArray(stores)) {
-      const {stores, searchLocation} = this.props;
+      const { stores, searchLocation } = this.props;
 
       mapUrl = this.createMapUrl(searchLocation.name, stores);
-    };
+    }
 
     return (
       <div>
-        <Header/>
-        <SearchForm><Input/></SearchForm>
-        {mapUrl !== null
-          ? <StoreResults
+        <Header />
+        <LayoutContainer backgroundColor={"#f0f7ed"}>
+          <h2 className="header__description">
+            Find your local store and its opening times
+          </h2>
+          <SearchForm updateOnSubmit={this.formSubmitHandler} />
+        </LayoutContainer>
+        <LayoutContainer>
+          {mapUrl !== null ? (
+            <StoreResults
               mapUrl={mapUrl}
               stores={stores}
-              searchLocation={searchLocation.name}/>
-          : ''}
+              searchLocation={searchLocation.name}
+            />
+          ) : (
+            ""
+          )}
+        </LayoutContainer>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({searchLocation: state.location, stores: state.storeList, storeMaps: state.storeMaps});
+const mapStateToProps = state => ({
+  searchLocation: state.location,
+  stores: state.storeList,
+  storeMaps: state.storeMaps
+});
 
 export default connect(mapStateToProps)(FindStores);
